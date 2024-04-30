6b191b442dcaf47f30594007dc80f3179ea55e6e
Damiil Shirokov <daniil196196@gmail.com>
2024-05-01 00:40:53 +0500

    clear code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,57 +3,49 @@ package org.example;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    public static StringBuffer infoAboutDownload = new StringBuffer("");
     public static void main(String[] args) throws IOException {
-        StringBuffer infoAboutDownload = new StringBuffer("");
-        File src = new File("F:\\Work\\JavaNetology\\Games\\src");
-        File srcMain = new File("F:\\Work\\JavaNetology\\Games\\src\\main");
-        File srcMainMain = new File("F:\\Work\\JavaNetology\\Games\\src\\main",  "Main.java");
-        File srcMainUtils = new File("F:\\Work\\JavaNetology\\Games\\src\\main",  "Utils.java");
-        File srcTest = new File("F:\\Work\\JavaNetology\\Games\\src\\test");
-        File savegames = new File("F:\\Work\\JavaNetology\\Games\\savegames");
-        File res = new File("F:\\Work\\JavaNetology\\Games\\res");
-        File resDrawables = new File("F:\\Work\\JavaNetology\\Games\\res\\drawables");
-        File resVectors = new File("F:\\Work\\JavaNetology\\Games\\res\\vectors");
-        File resIcons = new File("F:\\Work\\JavaNetology\\Games\\res\\icons");
-        File temp = new File("F:\\Work\\JavaNetology\\Games\\temp");
-        //File tempTxt = new File("F:\\Work\\JavaNetology\\Games\\temp", "temp.txt");
+        List<String> listFile = new ArrayList<>();
+        listFile.add("F:\\Work\\JavaNetology\\Games\\src");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\src\\main");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\src\\main\\Main.java");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\src\\main\\Utils.java");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\src\\test");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\savegames");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\res");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\res\\drawables");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\res\\vectors");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\res\\icons");
+        listFile.add("F:\\Work\\JavaNetology\\Games\\temp");
+
+        for (String file : listFile) {
+            createFile(file);
+        }
+        createLogFile();
+    }
+
+    public static void createFile(String filePath) {
+        File file = new File(filePath);
+        if (file.mkdir())
+            createLogRecord(file);
+    }
+
+    public static void createLogRecord (File file) {
+        infoAboutDownload.append( file.getName() + "создан в " + file.getPath() + "\n");
+    }
+
+    public static void createLogFile() {
         try {
-            if (src.mkdir()) {
-                infoAboutDownload.append("Папка src создана \n");
-                if (srcMain.mkdir())
-                    infoAboutDownload.append("Папка main в директории src создана \n");
-                    if (srcMainMain.createNewFile())
-                        infoAboutDownload.append("Файл Main.java в директории src\\main создан \n");
-                    if (srcMainUtils.createNewFile())
-                        infoAboutDownload.append("Файл Utils.java в директории src\\main создан \n");
-                if (srcTest.mkdir())
-                    infoAboutDownload.append("Папка test в директории src создана \n");
-            }
-
-            if (savegames.mkdir())
-                infoAboutDownload.append("Папка savegames создана \n");
-            if (res.mkdir()) {
-                infoAboutDownload.append("Папка res создана \n");
-                if (resDrawables.mkdir())
-                    infoAboutDownload.append("Папка drawables в директории res создана \n");
-                if (resVectors.mkdir())
-                    infoAboutDownload.append("Папка vectors в директории res создана \n");
-                if (resIcons.mkdir())
-                    infoAboutDownload.append("Папка icons в директории res создана \n");
-
-            }
-            if (temp.mkdir())
-                infoAboutDownload.append("Папка temp создана \n");
-
-
             FileWriter writer = new FileWriter("F:\\Work\\JavaNetology\\Games\\temp\\temp.txt");
             writer.write(infoAboutDownload.toString());
             writer.close();
         } catch (IOException e) {
-            System.out.println("Ошибка при записи в файл");
-            e.printStackTrace();
+              System.out.println("Ошибка при записи в файл");
+               e.printStackTrace();
         }
     }
 }
